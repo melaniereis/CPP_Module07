db79dca667c9284bb2ccd3f68058d28c6a128dce
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-05-24 16:32:02 +0100

    implement ex01

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -3,16 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: meferraz <meferraz@student.42porto.pt>     #+#  +:+       +#+        */
+/*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-05-24 10:42:57 by meferraz          #+#    #+#             */
-/*   Updated: 2025-05-24 10:42:57 by meferraz         ###   ########.fr       */
+/*   Created: 2025/05/24 10:42:57 by meferraz          #+#    #+#             */
+/*   Updated: 2025/05/24 16:31:12 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <string>
+#include "ansi.h"
 
 template <typename T>
-void iter(T[] &array, )
+void iter(T *array, const size_t size, void (*f)(const T &))
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		(*f)(array[i]);
+	}
+}
+
+template <typename T>
+void printArray(T *array)
+{
+	for (size_t i = 0; i < (sizeof(array) / sizeof(array[0])); i++)
+	{
+		std::cout << array[i] << " * ";
+	}
+	std::cout << std::endl;
+}
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,24 +6,39 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:33:55 by meferraz          #+#    #+#             */
-/*   Updated: 2025/05/24 08:07:36 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/05/24 16:30:57 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/iter.hpp"
 
-#define SEPARATOR(txt) std::cout << "\n" \
-<< BWHT "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n" \
-<< "📄 " << BBLU << txt << BWHT "\n" \
-<< "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" RESET "\n" << std::endl;
+#define SEPARATOR(txt) std::cout << "\n"                                              \
+								 << BWHT "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"     \
+								 << "📄 " << BBLU << txt << BWHT "\n"                 \
+								 << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" RESET "\n" \
+								 << std::endl;
 
-//Functions Prototype
+// Functions Prototype
 void test_subject_case();
 
 int main()
 {
 	std::cout << BGRN "\n\n📋===== SWAP, MIN, MAX W/TEMPLATES =====📋\n\n" RESET;
 
+	SEPARATOR("Test with int array")
+	int intArray[] = {1, 2, 3, 4, 5};
+	iter(intArray, 5, printArray<int>);
+	std::cout << std::endl;
+
+	SEPARATOR("Test with float array")
+	float floatArray[] = {1.1f, 2.2f, 3.3f};
+	iter(floatArray, 3, printArray<float>);
+	std::cout << std::endl;
+
+	SEPARATOR("Test with std::string array")
+	std::string strArray[] = {"42", "Porto", "Piscine", "C++"};
+	iter(strArray, 4, printArray<std::string>);
+	std::cout << std::endl;
 
 	std::cout << BGRN "\n\n✅ All tests complete!\n\n" RESET;
 	return 0;
