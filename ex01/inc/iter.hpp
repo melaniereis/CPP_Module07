a54ef7c4d051d6259744242385c8dad794434024
Melanie Ferraz Reis <meferraz@c1r2s6.42porto.com>
2025-05-31 11:57:32 +0100

    add iter version for non const type

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: meferraz <meferraz@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 10:42:57 by meferraz          #+#    #+#             */
-/*   Updated: 2025/05/30 21:18:01 by meferraz         ###   ########.fr       */
+/*   Updated: 2025/05/31 11:57:05 by meferraz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,21 @@ void iter(T *array, const size_t size, void (*f)(const T &))
 }
 
 /**
+ * Applies a given function to each element of an array, modifying the elements.
+ *
+ * @tparam T The type of the elements in the array.
+ * @param array A pointer to the first element of the array.
+ * @param size The number of elements in the array.
+ * @param f A function pointer that takes a reference to an element of type T and returns void.
+ */
+template <typename T>
+void iter(T *array, const size_t size, void (*f)(T &))
+{
+	for (size_t i = 0; i < size; i++)
+		(*f)(array[i]);
+}
+
+/**
  * Prints a given element of an array to the standard output.
  *
  * @param element A constant reference to the element to print.
@@ -41,4 +56,3 @@ void printElementOfArray(const T &element)
 {
 	std::cout << YEL << element << RESET << " * ";
 }
-
